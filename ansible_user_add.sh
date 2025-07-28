a68e49229ed5abdf2912e4de81523c337f53f5e5
Artem <artemik@yahoo.com>
2025-07-28 07:43:06 +0200

    Create ansible_user_add.sh

---
+++ ansible_user_add.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Variables
+USERNAME="ansible-user"
+PASSWORD="ansible"
+
+useradd -m -s /bin/bash "$USERNAME"
+
+echo "$USERNAME:$PASSWORD" | chpasswd
+
+# Ubuntu/Debian
+usermod -aG sudo "$USERNAME"
+
+# Rocky/CentOS/Fedora/RHEL
+# usermod -aG wheel "$USERNAME"
+
+chown -R "$USERNAME:$USERNAME" /home/"$USERNAME"
+chown 755 /home/"$USERNAME"
+
+echo "User $USERNAME is ready!"
