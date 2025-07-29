cbec06c6f9679883505a60cc38fd936fe6f1b1d9
Artem <artemik@yahoo.com>
2025-07-29 12:09:53 +0200

    Create ansible_install.sh

---
+++ ansible_install.sh
@@ -0,0 +1,10 @@
+# The latest version can only be obtained via the Python package manager
+# Example 29.07.2025: v.2.19.0 via pip (21.07.2025), v.2.16.3 via apt (29.01.2024!)
+
+sudo python3 -m venv ansible-env
+
+source ansible-env/bin/activate
+
+#deactivate
+
+pip3 install ansible-core
