77c5ab2ea3485689256b5e5a4cb1d2bae8781385
Artem <artemik@yahoo.com>
2025-08-04 14:36:10 +0200

    Update ansible_install.sh: run without sudo

--- ansible_install.sh
+++ ansible_install.sh
@@ -1,10 +1,11 @@
 # The latest version can only be obtained via the Python package manager
 # Example 29.07.2025: v.2.19.0 via pip (21.07.2025), v.2.16.3 via apt (29.01.2024!)
 
-sudo python3 -m venv ansible-env
+python3 -m venv ansible-env
 
 source ansible-env/bin/activate
-
-#deactivate
+# Exit: deactivate
 
 pip3 install ansible-core
+
+pip install --upgrade pip
